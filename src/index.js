207f078419da872e215bc9a593e81f6bea626892
kamil amin <Kamil.amin@live.co.uk>
2017-02-18 21:01:02 +0500

    React Tooltip app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render( <div>
+  <App text='Master Express.js-The Node.js Framework For you Web Development'>Pro Express.js </App>
+    was published in 2014, it was one of the first book on v4.x. And it was my second book published with Apress
+    after <App text='Practical Node.js: Building Real-world Scalable Web Apps'>Practical Node.js</App>.. The main Focus of this post is to compare the
+    four Node.js/Io.js frameworks: <App text='HTTP API Server'>Hapi</App>, <App text='Release the Kraken!!'>Kraken</App>, <App text='Sail away'>Sails.js</App>
+     and <App text='IBM of Framework'>Loopback</App>. There are man other frameworks to consider, but i had to draw the somewhere.
+  </div>,
+  document.getElementById('root')
+);
